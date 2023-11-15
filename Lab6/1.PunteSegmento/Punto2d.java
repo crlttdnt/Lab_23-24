f04eee64b254ad20196fe53b2a02b895115cb027
crlttdnt <carlotta.donato03@gmail.com>
2023-11-15 13:50:39 +0100

    lab6

---
+++ Lab6/1.PunteSegmento/Punto2d.java
@@ -0,0 +1,19 @@
+public class Punto2d {
+    //OVERVIEW: modella un punto in 2D - immutabile
+    public final double x,y; //final --> immutabili
+
+    //costruttore
+    public Punto2d(double x, double y) {
+        //MODIFIES: this
+        //EFFECTS: inizializza this con x e y
+        this.x = x;
+        this.y = y;
+
+    }
+
+
+
+
+
+
+}
