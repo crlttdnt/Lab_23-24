37c9ce2b4268876dd29af47e53bbb68e27a8997d
crlttdnt <carlotta.donato03@gmail.com>
2023-11-22 17:09:13 +0100

    Menu lab5

--- Lab5/4.Menu/Menu.java
+++ Lab5/4.Menu/Menu.java
@@ -1,4 +1,20 @@
 public class Menu implements Iterable<Piatto> {
 // OVERVIEW: Classe che contiene una serie di piatti. Iterabile su tale insieme.
 
+//attributi
+    private ArrayList<Piatto> piatti;
+
+//costruttori
+public Menu() {
+    // MODIFIES: this
+    // EFFECTS: istanza un nuovo Menu vuoto
+    piatti = new ArrayList<>();
+
+    assert repOk();
+}
+
+
+//metodi
+
+
 }
